2076bb5ef9c2ee8042d7f243a286bcdaa053e92c
Leira Hua <39118+leira@users.noreply.github.com>
2023-01-07 23:46:59 -0800

    Change the left thumb Tab key to Space key.
    I almost never used the thumb Tab key, but I occationally mis-hit it as a space key.

--- keyboards/beekeeb/piantor/keymaps/leira-steno/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/leira-steno/keymap.c
@@ -33,25 +33,25 @@ enum layer_names {
 #define T_PLOVER TO(_PLOVER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /*
-      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │Tab│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
-      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Ctl│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
-      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Sft│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Sft│
-      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
-      *               ┌───┐                   ┌───┐
-      *               │GUI├───┐           ┌───┤Alt│
-      *               └───┤Bsp├───┐   ┌───┤Ent├───┘
-      *                   └───┤   │   │   ├───┘
-      *                       └───┘   └───┘
-      */
+    /*
+    * ┌───────┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───────┐
+    * │Tab|Alt│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │  Bsp  │
+    * ├───────┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───────┤
+    * │Esc|Ctl│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' |FUN│
+    * ├───────┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───────┤
+    * │ ` |Sft│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │ \ |Sft│
+    * └───────┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───────┘
+    *                   ┌───┐                   ┌───┐
+    *                   │GUI├───┐           ┌───┤Alt│
+    *                   └───┤NUM├───┐   ┌───┤SYM├───┘
+    *                       └───┤   │   │   ├───┘
+    *                           └───┘   └───┘
+    */
     [_QWERTY] = LAYOUT_split_3x6_3(
         LALT_TAB,KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         LCTL_ESC,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, FUN_QUOT,
         LSFT_GRV,KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  MED_SLSH,RSFT_BSL,
-                                            KC_LGUI, NUMBER,  KC_TAB,    KC_SPC,  SYMBOL,  KC_ENT
+                                            KC_LGUI, NUMBER,  KC_SPC,    KC_SPC,  SYMBOL,  KC_ENT
     ),
 
     [_NUMBER] = LAYOUT_split_3x6_3(
